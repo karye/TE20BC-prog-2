b47ddd31c28846da1063615b22962a9727a86c7f
Karye <karye@users.noreply.github.com>
2022-09-12 14:03:22 +0200

    Tränat på XAML, klick-event och name

---
+++ WPF/StackPanel/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace StackPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void KlickSkicka(object sender, RoutedEventArgs e)
+        {
+            if (rutaEpost.Text == "" || rutaTexten.Text == "")
+            {
+                rutaStatus.Text = "Fel! Vg fyll epost och text...";
+            }
+            else
+            {
+                rutaStatus.Text = "Meddelande har skickats!";
+            }
+        }
+    }
+}
