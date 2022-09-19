53ac25689d46e8b9ccf4c016dac3576998b55a1b
Karye <karye@users.noreply.github.com>
2022-09-19 14:06:54 +0200

    Klar

--- WPF/BrannbollRaknare/MainWindow.xaml.cs
+++ WPF/BrannbollRaknare/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using System.Timers;
 
 namespace BrannbollRaknare
 {
@@ -27,6 +17,20 @@ namespace BrannbollRaknare
         public MainWindow()
         {
             InitializeComponent();
+            Timer timer = new Timer(1000);
+            timer.Elapsed += OnTimerTick;
+            timer.Start();
+        }
+
+        private void OnTimerTick(object source, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(UpdateraKlocka);
+        }
+
+        private void UpdateraKlocka()
+        {
+            DateTime nu = DateTime.Now;
+            rutaKlocka.Text = nu.ToString("HH:mm:ss");
         }
 
         // Hantera klick på knappen Frivarv
@@ -37,27 +41,55 @@ namespace BrannbollRaknare
 
             // Skriv innelaget poäng
             rutaInne.Text = innePoäng.ToString();
+
+            // Skriv ut i rutan historik
+            DateTime nu = DateTime.Now;
+            rutaHistorik.Text += nu.ToString("HH:mm:ss") + " Frivarv +5 poäng\n";
+
+            // Spara ned i en textfil
+            File.WriteAllText("historik.txt", rutaHistorik.Text);
         }
 
         // Hantera klick på knappen Frivarv
         private void KlickBränning(object sender, RoutedEventArgs e)
         {
             utePoäng += 2;
-            rutaUte.Text = "2";
+            rutaUte.Text = utePoäng.ToString();
+
+            // Skriv ut i rutan historik
+            DateTime nu = DateTime.Now;
+            rutaHistorik.Text += nu.ToString("HH:mm:ss") + " Bränning +2 poäng\n";
+
+            // Spara ned i en textfil
+            File.WriteAllText("historik.txt", rutaHistorik.Text);
         }
 
         // Hantera klick på knappen Frivarv
         private void KlickLyra(object sender, RoutedEventArgs e)
         {
             utePoäng += 3;
-            rutaUte.Text = "3";
+            rutaUte.Text = utePoäng.ToString();
+
+            // Skriv ut i rutan historik
+            DateTime nu = DateTime.Now;
+            rutaHistorik.Text += nu.ToString("HH:mm:ss") + " Lyra +3 poäng\n";
+
+            // Spara ned i en textfil
+            File.WriteAllText("historik.txt", rutaHistorik.Text);
         }
 
         // Hantera klick på knappen Frivarv
         private void KlickVarv(object sender, RoutedEventArgs e)
         {
             innePoäng += 1;
-            rutaInne.Text = "1";
+            rutaInne.Text = innePoäng.ToString();
+
+            // Skriv ut i rutan historik
+            DateTime nu = DateTime.Now;
+            rutaHistorik.Text += nu.ToString("HH:mm:ss") + " Varv +1 poäng\n";
+
+            // Spara ned i en textfil
+            File.WriteAllText("historik.txt", rutaHistorik.Text);
         }
     }
 }
