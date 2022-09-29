4eb70aeeab77ee3d83a00bb50de230fb7021e6bc
Karye <karye@users.noreply.github.com>
2022-09-29 11:10:29 +0200

    Klar med programmet

--- WPF/Gridlayout/MainWindow.xaml.cs
+++ WPF/Gridlayout/MainWindow.xaml.cs
@@ -4,14 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Gridlayout
 {
@@ -23,6 +16,34 @@ namespace Gridlayout
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Hantera "event" Click på knappen Skicka!
+        private void KlickSkicka(object sender, RoutedEventArgs e)
+        {
+            // Läs av inputrutorna
+            string epost = rutaEpost.Text;
+            string texten = rutaTexten.Text;
+
+            // 1:a kontrollen - inte tomt
+            if (epost == "" || texten == "")
+            {
+                rutaStatus.Text = "Fel! Epost eller text saknas";
+            }
+            else
+            {
+                // 2:a kontrollen - epost rätt format?
+                Regex regexEpost = new Regex("\\S+@\\S+\\.\\S+");
+                if (!regexEpost.IsMatch(epost))
+                {
+                    rutaStatus.Text = "Fel! Epost ogiltigt format";
+                }
+                else
+                {
+                    rutaStatus.Text = "Epost skickad";
+                }
+            }
+
         }
     }
 }
