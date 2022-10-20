40ac7c8a1840b30d394ff1c86407b6073bc5d415
karye <karye.webb@gmail.com>
2022-10-20 11:10:02 +0200

    Tränat på klasser

---
+++ Klasser/Intro/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Räkna ut lönen!");
+
+            // Person1
+            // Ange ditt namn
+            Console.Write("Ange ditt namn: ");
+            string namn1 = Console.ReadLine();
+            
+            // Timlön?
+            Console.Write("Ange din timlön: ");
+            int timlön1 = int.Parse(Console.ReadLine());
+
+            // Antal timmar
+            Console.Write("Ange antal timmar du jobbat: ");
+            int antalTimmar1 = int.Parse(Console.ReadLine());
+
+            // Svaret
+            Console.WriteLine($"{namn1} din totala lön blir {timlön1 * antalTimmar1}");
+
+            // Person2
+            // Ange ditt namn
+            Console.Write("Ange ditt namn: ");
+            string namn2 = Console.ReadLine();
+            
+            // Timlön?
+            Console.Write("Ange din timlön: ");
+            int timlön2 = int.Parse(Console.ReadLine());
+
+            // Antal timmar
+            Console.Write("Ange antal timmar du jobbat: ");
+            int antalTimmar2 = int.Parse(Console.ReadLine());
+
+            // Svaret
+            Console.WriteLine($"{namn2} din totala lön blir {timlön2 * antalTimmar2}");
+
+            // Person3
+            // Ange ditt namn
+            Console.Write("Ange ditt namn: ");
+            string namn3 = Console.ReadLine();
+            
+            // Timlön?
+            Console.Write("Ange din timlön: ");
+            int timlön3 = int.Parse(Console.ReadLine());
+
+            // Antal timmar
+            Console.Write("Ange antal timmar du jobbat: ");
+            int antalTimmar3 = int.Parse(Console.ReadLine());
+
+            // Svaret
+            Console.WriteLine($"{namn3} din totala lön blir {timlön3 * antalTimmar3}");
+        }
+    }
+}
---
+++ Klasser/Intro2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Intro2
+{
+    // Mall
+    class Person
+    {
+        public string namn;
+        public int timlön;
+        public int antalTimmar;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Räkna ut löner!");
+
+            // Person1
+            Person person1 = new Person();  // Instansierar objektet/instans 
+            Person person2 = new Person();
+            Person person3 = new Person();
+
+            Console.Write("Ange ditt namn: ");
+            person1.namn = Console.ReadLine();
+
+            Console.Write("Ange din timlön: ");
+            person1.timlön = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange antal timmar: ");
+            person1.antalTimmar = int.Parse(Console.ReadLine());
+        }
+    }
+}
