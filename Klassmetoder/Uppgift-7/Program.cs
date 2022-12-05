d7e674aa36a881d9263d2278f4fd363f2f368c6d
karye <karye.webb@gmail.com>
2022-12-05 13:27:18 +0100

    Tränat på konstruktor

---
+++ Klassmetoder/Uppgift-7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_7
+{
+    class Elev
+    {
+        public string förnamn;
+        public string efternamn;
+        public DateTime födelseDatum;
+        public double längd;
+        public double vikt;
+
+        public bool Myndig()
+        {
+            int age = DateTime.Now.Year - födelseDatum.Year;
+            return age >= 18;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Student!");
+        }
+    }
+}
