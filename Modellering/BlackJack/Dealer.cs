da397e8ec73d60eb829b8221d96514f7b4fc7535
karye <karye.webb@gmail.com>
2023-03-27 13:29:37 +0200

    Börjat med spellogiken

---
+++ Modellering/BlackJack/Dealer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BlackJack
+{
+    public class Dealer : Spelare
+    {
+        public Dealer(string n) : base (n)
+        {
+            
+        }
+    }
+}
--- Modellering/BlackJack/Program.cs
+++ Modellering/BlackJack/Program.cs
@@ -7,44 +7,97 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.WriteLine("Välkommen till Black Jack!");
 
-            // Skriv ut alla kort i kortleken
-            // Steg 1 Skapa ett objekt = kortlek
-            KortLek kortlek = new KortLek();
+            // Skapa en spelare med ett namn
+            Console.Write("Vad heter du? ");
+            string namn = Console.ReadLine();
+            Spelare spelare1 = new Spelare(namn);
 
-            // Steg 2 - fyll med alla 52 kort
+            // Skapa en dealer
+            Dealer dealer = new Dealer("Dealer");
+
+            // Skapa en kortlek med 52 kort
+            KortLek kortlek = new KortLek();
             kortlek.SkapaKortLek();
 
-            // Steg 3 - blanda
+            // Blanda kortleken
             kortlek.BlandaKortlek();
 
-            // Steg 4 
-           /* foreach (var kort in  kortlek.GeKortlek())
-           {
-                Console.WriteLine(kort.TillText());
-           } */
+            // Spelomgång börjar (spelloopen)
+            string spelStatus = "spelar";
+            while (spelStatus == "spelar")
+            {
+                // Ge två kort till spelaren
+                // Alt 1: skapa ett kort, sen ge kortet
+                Kort ettKort = kortlek.DraKort();
+                spelare1.TaEmotKort(ettKort);
+                // Alt 2: ge kortet direkt
+                spelare1.TaEmotKort(kortlek.DraKort());
+                // Spelaren har nu 2 kort i handen
 
-           // Testa att dra ett kort o skriv ut det
-           Kort ettKort = kortlek.DraKort();
-           Console.WriteLine(ettKort.TillText());
+                // .. och två kort till dealern
+                dealer.TaEmotKort(kortlek.DraKort());
+                dealer.TaEmotKort(kortlek.DraKort());
 
-           // Skapa en spelare med ett namn
-           Spelare spelare1 = new Spelare("Kalle");
+                // Visa spelarens kort
+                Console.Write("Dina två kort är: ");
+                foreach (var kort in spelare1.Hand())
+                {
+                    Console.Write(kort.TillText() + ", ");
+                }
+                Console.WriteLine($"\nPoäng: {spelare1.Poäng()}");
 
-           // Ge spelare1 det dragna kortet
-           spelare1.TaEmotKort(ettKort);
+                // Om spelaren får 21, vinner spelaren direkt.
+                if (spelare1.Poäng() == 21)
+                {
+                    Console.WriteLine("Du har Black Jack och du har vunnit spelomgången!");
+                    spelStatus = "vunnit";
+                    break;
+                }
+                else if (dealer.Poäng() == 21)
+                {
+                    Console.WriteLine("Dealern har Black Jack och du har forlorat spelomgången!");
+                    spelStatus = "förlorat";
+                    break;
+                }
 
-           // Skriv spelare1 totala poäng
-           Console.WriteLine(spelare1.Poäng());
+                // Visa dealerns första kort
+                Console.WriteLine("Dealerm kort är: " + dealer.Hand()[0].TillText());
 
-           // Dra ett till kort och ge till spelare1
-           ettKort = kortlek.DraKort();
-           Console.WriteLine(ettKort.TillText());
-           spelare1.TaEmotKort(ettKort);
-           Console.WriteLine(spelare1.Poäng());
+                while (true)
+                {
+                    // Spelaren får välja att dra kort eller stanna. Om spelaren stannar, går turen vidare till dealern.
+                    Console.Write("Vill du dra ett kort till (j/n): ");
+                    string val = Console.ReadLine();
+                    if (val.ToLower() == "j")
+                    {
+                        spelare1.TaEmotKort(kortlek.DraKort());
+                        Console.Write("Dina kort är: ");
+                        foreach (var kort in spelare1.Hand())
+                        {
+                            Console.Write(kort.TillText() + ", ");
+                        }
+                        Console.WriteLine($"\nPoäng: {spelare1.Poäng()}");
 
-           
+                        // Om spelare får över 21 förlorar hen
+                        if (spelare1.Poäng() > 21)
+                        {
+                            Console.WriteLine("Du förlorar!");
+                            spelStatus = "förlorat";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                Console.ReadLine();
+                
+            }
         }
     }
 }
