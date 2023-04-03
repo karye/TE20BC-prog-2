e8f1e626da0345c4aee79cf6ec7c72d46e2a2186
karye <karye.webb@gmail.com>
2023-04-03 13:34:02 +0200

    Byggt gränssnittet

---
+++ WPF/MediaRegsiter/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaRegister
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Metoder för alla events från gränssnittet
+        // Hantera Spara film
+        private void KlickSparaFilm(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        // Hantera Spara film
+        private void KlickSparaBok(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        // Hantera klick på radioknapp
+        private void CheckRadio(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
